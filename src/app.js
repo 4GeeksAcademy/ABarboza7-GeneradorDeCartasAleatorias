38ec5bc766901d59d85c70e97fd49aeda246ecbc
ABarboza7 <164107449+ABarboza7@users.noreply.github.com>
2026-01-12 21:35:29 +0000

    cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import "bootstrap";
 import "./style.css";
 
+const numeros = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
+const cartas = ["♥","♦","♣","♠"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function mostrarCarta() {
+  const valor = numeros[Math.floor(Math.random() * numeros.length)];
+  const tipoDeCarta = cartas[Math.floor(Math.random() * cartas.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("contenedor").innerHTML =
+    `<div class="carta">
+       <div class="esquina arriba-izq">${valor}${tipoDeCarta}</div>
+       <div class="centro">${valor}${tipoDeCarta}</div>
+       <div class="esquina abajo-der">${valor}${tipoDeCarta}</div>
+     </div>`;
+}
+
+
+window.mostrarCarta = mostrarCarta;
+
