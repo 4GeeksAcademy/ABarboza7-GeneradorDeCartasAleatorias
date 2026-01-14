e11bdc86f331300d9c26fcc6585b483ec74adfa8
ABarboza7 <164107449+ABarboza7@users.noreply.github.com>
2026-01-14 18:32:02 +0000

    add funcionalidad de changeSize para dimencionar cartas y add setInterval para mostar una carta cada cierto tiempo

--- src/app.js
+++ src/app.js
@@ -1,21 +1,44 @@
 import "bootstrap";
 import "./style.css";
 
-const numeros = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"];
-const cartas = ["♥","♦","♣","♠"];
+const numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const cartas = ["♠", "♥", "♦", "♣"];
 
 function mostrarCarta() {
   const valor = numeros[Math.floor(Math.random() * numeros.length)];
   const tipoDeCarta = cartas[Math.floor(Math.random() * cartas.length)];
 
-  document.getElementById("contenedor").innerHTML =
-    `<div class="carta">
-       <div class="esquina arriba-izq">${valor}${tipoDeCarta}</div>
-       <div class="centro">${valor}${tipoDeCarta}</div>
-       <div class="esquina abajo-der">${valor}${tipoDeCarta}</div>
-     </div>`;
+  document.getElementById("contenedor").innerHTML = `
+    <div class="carta">
+      <div class="esquina arriba-izq">${valor}${tipoDeCarta}</div>
+      <div class="centro">${valor}${tipoDeCarta}</div>
+      <div class="esquina abajo-der">${valor}${tipoDeCarta}</div>
+    </div>
+  `;
 }
 
+function chageSize() {
+  const alto = document.getElementById("alto").value;
+  const ancho = document.getElementById("ancho").value;
+
+  const carta = document.querySelector(".carta");
+
+  if (!carta) {
+    alert("Primero sacá una carta");
+    return;
+  }
+
+  if (alto) {
+    carta.style.height = alto + "px";
+  }
+
+  if (ancho) {
+    carta.style.width = ancho + "px";
+  }
+}
 
 window.mostrarCarta = mostrarCarta;
+window.chageSize = chageSize;
+
+setInterval(mostrarCarta, 10000);
 
